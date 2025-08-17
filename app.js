8337286a88269aa0ab5c79ee7b93ec5b1f2d9ec2
vaibhav agrawal <vaibhavagrawal264@gmail.com>
2025-08-17 16:08:15 +0530

    Connecting with the Database

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const express=require('express')
 const app=express()
-
+const connectDB=require('./db/connect')
 const tasks=require('./routes/tasks')
 app.use(express.json())
-
+require('dotenv').config()
 app.get('/',(req,res)=>{
     res.json({success:true,msg:"Hello World"})
 })
 
 app.use('/api/v1/tasks',tasks)
+const port=3000
+const start= async()=>{
+    try{
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port,(req,res)=>{
+            console.log(`Server is running on port ${port}`)
+        })
 
-app.listen(3000,(req,res)=>{
-    console.log("Server is running on port 3000")
-})
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+start()
